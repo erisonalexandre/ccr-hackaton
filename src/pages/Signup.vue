d8b961973d53b25a4da56f41eb066184c26e2c96
erison <erison@flexpeak.com.br>
2020-06-14 00:23:09 -0400

    [add] Signup page

--- src/pages/Signup.vue
+++ src/pages/Signup.vue
@@ -1,8 +1,30 @@
 <template>
-  <!-- <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page> -->
-  <h1>Sign Up Page</h1>
+  <div class="container">
+    <div class="row align-center">
+      <h4 class="col-8"><b>Cadastre-se</b></h4>
+      <q-avatar class="background-yellow" text-color="white" icon="person" size="100px"/>
+    </div>
+
+    <div>
+      <q-form>
+        <div class="mb-3">
+          <q-input v-model="text" filled label="Nome" />
+        </div>
+        <div class="mb-3">
+          <q-input v-model="text" filled label="Sobrenome" />
+        </div>
+        <div class="mb-3">
+          <q-input v-model="text" filled label="Data de nascimento" />
+        </div>
+        <div class="mb-3">
+          <q-input v-model="text" filled label="Placa de caminhão" />
+        </div>
+        <div class="mt-40">
+          <q-btn label="Avançar" type="submit" rounded class="w-100 background-yellow text-white"/>
+        </div>
+      </q-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,3 +32,24 @@ export default {
   name: 'PageSignUp'
 }
 </script>
+
+<style lang="scss">
+  .container {
+    padding: 0px 10px;
+  }
+  .mb-3 {
+    margin-bottom: 15px;
+  }
+  .mt-40 {
+    margin-top: 40px;
+  }
+  .w-100 {
+    width: 100%;
+  }
+  .background-yellow {
+    background-color: #F1C232
+  }
+  .q-btn__wrapper:before {
+    box-shadow: none;
+  }
+</style>
