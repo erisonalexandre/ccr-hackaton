6e4d5ab2f3bdfd86f51e89322769cce531f81a83
David Alen <davidalen51@gmail.com>
2020-06-14 18:01:44 -0300

    [add] app config information

--- quasar.conf.js
+++ quasar.conf.js
@@ -153,7 +153,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/developing-cordova-apps/configuring-cordova
     cordova: {
       // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
-      id: 'org.cordova.quasar.app'
+      id: 'com.br.ccr.minhaestrada'
     },
 
     // Full list of options: https://quasar.dev/quasar-cli/developing-capacitor-apps/configuring-capacitor
