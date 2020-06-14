30fbe156187bd9e772f70080fbd5f987c84c876b
David Alen <davidalen51@gmail.com>
2020-06-14 07:13:05 -0300

    [fix] emergency page

--- src/pages/Emergency.vue
+++ src/pages/Emergency.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="container">
-    <q-btn class="item" color="black" label="Polícia" />
-    <q-btn class="item" color="red" label="Central CCR" />
-    <q-btn class="item" color="green" label="Suporte via WhatsApp" />
+    <span class="title">
+      Precisando de ajuda?
+    </span>
+    <span class="subtitle">
+      Toque em um dos contatos abaixo!
+    </span>
+    <q-btn type="a" href="tel:190" class="item" color="black" label="Polícia" />
+    <q-btn type="a" href="tel:0800000290" class="item" color="red" label="Central CCR" />
+    <q-btn type="a" href="whatsapp://send?abid=+55996265253&text=Eu%20preciso%20de%20ajuda!" class="item" color="green" label="Suporte via WhatsApp" />
   </div>
 </template>
 
@@ -17,13 +23,34 @@ export default {
     display: flex;
     min-height: calc(100vh - 72px);
     flex-direction: column;
-    justify-content: center;
-
+    justify-content: start;
+    background-color: #9E260E;
   }
 
   .item {
-    margin: 10px 20px;
-    height: 8vh;
+    margin: 24px;
+    padding: 24px 0;
     min-height: 50px;
+    transition: box-shadow 0.3s cubic-bezier(0.25, 0.8, 0.5, 1);
+  }
+
+  .item:active {
+    box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 5px 8px rgba(0, 0, 0, 0.14), 0 1px 14px rgba(0, 0, 0, 0.12);
+  }
+
+  .title {
+    text-align: center;
+    padding: 20px 0;
+    font-size: 40px;
+    align-self: center;
+    color: #F1C232;
+  }
+
+  .subtitle {
+    text-align: center;
+    padding: 10px 0;
+    font-size: 25px;
+    align-self: center;
+    color: white;
   }
 </style>
