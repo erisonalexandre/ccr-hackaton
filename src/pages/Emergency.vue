7793d7a8ab739f8dc17e516a52a65bf9a54b46aa
David Alen <davidalen51@gmail.com>
2020-06-13 23:48:08 -0300

    [add] emergy static page

--- src/pages/Emergency.vue
+++ src/pages/Emergency.vue
@@ -1,8 +1,9 @@
 <template>
-  <!-- <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
-  </q-page> -->
-  <h1>Emergency Page</h1>
+  <div class="container">
+    <q-btn class="item" color="black" label="Polícia" />
+    <q-btn class="item" color="red" label="Central CCR" />
+    <q-btn class="item" color="green" label="Suporte via WhatsApp" />
+  </div>
 </template>
 
 <script>
@@ -10,3 +11,19 @@ export default {
   name: 'PageEmergency'
 }
 </script>
+
+<style>
+  .container {
+    display: flex;
+    min-height: calc(100vh - 72px);
+    flex-direction: column;
+    justify-content: center;
+
+  }
+
+  .item {
+    margin: 10px 20px;
+    height: 8vh;
+    min-height: 50px;
+  }
+</style>
