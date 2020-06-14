d5e3ca15d487a87ebc03656c68ab1c985f7df778
erison <erison@flexpeak.com.br>
2020-06-14 00:26:51 -0400

    [fix] remove footer and essetialLinks from login layout

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -1,18 +1,5 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-footer elevated>
-      <q-tabs
-        v-model="tab"
-        indicator-color="yellow"
-        class="bg-primary text-white shadow-2"
-      >
-        <q-route-tab to="/Emergency" label="Emergência" />
-        <q-route-tab to="/Maps" label="Mapa" />
-        <q-route-tab to="/Health" label="Saúde" />
-        <q-route-tab to="/Profile" label="Perfil" />
-        <q-route-tab to="/Rewards" label="Recompensas" />
-      </q-tabs>
-    </q-footer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -20,63 +7,8 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
 
 export default {
-  name: 'LoginLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
-        },
-        {
-          title: 'Quasar Awesome',
-          caption: 'Community Quasar projects',
-          icon: 'favorite',
-          link: 'https://awesome.quasar.dev'
-        }
-      ]
-    }
-  }
+  name: 'LoginLayout'
 }
 </script>
