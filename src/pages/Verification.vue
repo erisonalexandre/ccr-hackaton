0f1c9051fa566733d4c1dcce67e4f8c15aea26b9
erison <erison@flexpeak.com.br>
2020-06-14 00:22:42 -0400

    [add] Verification page

---
+++ src/pages/Verification.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container text-center w-100" style="margin-top: 15%">
+    <div class="row align-center mb-40">
+      <h4 class="col-8 text-ccr-primary"><b>Verificação</b></h4>
+      <p>Digite o código de acesso de 6 digítos que chegou para você</p>
+    </div>
+
+    <div>
+      <q-form @submit.prevent="pass">
+        <div class="mb-40">
+          <q-input
+            filled
+            input-class="text-center"
+            class="text-center"
+            style="font-size:25px"
+            v-model="phone"
+            placeholder="1  1  1   1  1  1"
+            mask="#  #  #   #  #  #"
+            required
+          />
+        </div>
+        <div class="mt-40">
+          <q-btn label="Avançar" type="submit" rounded class="w-100 background-yellow text-white"/>
+        </div>
+      </q-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageSignUp',
+  methods: {
+    pass () {
+      this.$router.push({ name: 'Maps', params: { userId: '123' } })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .container {
+    padding: 0px 10px;
+  }
+  .mb-40 {
+    margin-bottom: 40px;
+  }
+  .mt-40 {
+    margin-top: 40px;
+  }
+  .w-100 {
+    width: 100%;
+  }
+  .background-yellow {
+    background-color: #F1C232
+  }
+  .text-ccr-primary {
+    color: #9E260E
+  }
+  .q-btn__wrapper:before {
+    box-shadow: none;
+  }
+</style>
