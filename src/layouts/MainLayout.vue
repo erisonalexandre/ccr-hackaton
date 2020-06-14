88f35630537c0b746f6c834eafb8a68fd21bb1a4
erison <erison@flexpeak.com.br>
2020-06-13 21:11:52 -0400

    [fix] icons menu and color

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,13 +5,13 @@
         v-model="tab"
         indicator-color="yellow"
         class="text-white shadow-2"
-        style="background-color: #F1C232"
+        style="background-color: #F1C232; text-size: 12px"
       >
-        <q-route-tab to="Emergency" label="Emergência" />
-        <q-route-tab to="Maps" label="Mapa" />
-        <q-route-tab to="Health" label="Saúde" />
-        <q-route-tab to="Profile" label="Perfil" />
-        <q-route-tab to="Rewards" label="Recompensas" />
+        <q-route-tab to="Health" icon="favorite" label="Saúde" />
+        <q-route-tab to="Profile" icon="account_circle" label="Perfil" />
+        <q-route-tab to="Maps" icon="room" label="Mapa" />
+        <q-route-tab to="Rewards" icon="emoji_events" label="Recompensas" />
+        <q-route-tab to="Emergency" icon="campaign" label="Emergência" />
       </q-tabs>
     </q-footer>
     <q-page-container>
@@ -25,3 +25,18 @@ export default {
   name: 'MainLayout'
 }
 </script>
+<style lang="scss">
+  a.q-tab {
+    padding: 0;
+  }
+  a.q-tab .q-tab__icon {
+    font-size: 34px;
+  }
+  a.q-tab .q-tab__label {
+    font-size: 9px;
+    padding-top: 7px;
+  }
+  .q-router-link--active {
+    color: #9E260E;
+  }
+</style>
