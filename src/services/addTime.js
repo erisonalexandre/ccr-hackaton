c03c1199e2245e12cfd66f88a650eb716a2f4a25
David Alen <davidalen51@gmail.com>
2020-06-14 04:37:45 -0300

    [add] addtime service

---
+++ src/services/addTime.js
@@ -0,0 +1,34 @@
+function addTimes (arr) {
+  var times = [ 0, 0, 0 ]
+  var max = times.length
+
+  var hours = times[0]
+  var minutes = times[1]
+  var seconds = times[2]
+
+  for (let time of arr) {
+    let splited = (time || '').split(':')
+    hours += isNaN(parseInt(splited[0])) ? 0 : parseInt(splited[0])
+    minutes += isNaN(parseInt(splited[1])) ? 0 : parseInt(splited[1])
+    seconds += isNaN(parseInt(splited[2])) ? 0 : parseInt(splited[2])
+  }
+
+
+  if (seconds >= 60) {
+    var m = (seconds / 60) << 0
+    minutes += m
+    seconds -= 60 * m
+  }
+
+  if (minutes >= 60) {
+    var h = (minutes / 60) << 0
+    hours += h
+    minutes -= 60 * h
+  }
+
+  return ('' + hours).padStart(2, '0') + ':' + ('0' + minutes).slice(-2) + ':' + ('0' + seconds).slice(-2)
+}
+
+export {
+  addTimes
+}
